9bded3202492ba8ae5b6542c5411a6bd6f513662
Ярослав Алексеев <allegendbull@yandex.ru>
2025-07-16 13:20:32 +0500

    Added dropdown menu to header. Small edit to keyframe fadeAwayLoader

---
+++ assets/js/dropdown.js
@@ -0,0 +1,21 @@
+export const dropdown = () => {
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(dropdown => {
+        const toggle = dropdown.querySelector('.dropdown__toggle');
+        const menu = dropdown.querySelector('.dropdown__list');
+
+        toggle.addEventListener('click', function () {
+            menu.classList.toggle('show');
+        });
+    });
+
+    window.addEventListener('click', function (e) {
+        if (!e.target.matches('.dropdown__toggle')) {
+            const openMenus = document.querySelectorAll('.dropdown__list.show');
+            openMenus.forEach(menu => {
+                menu.classList.remove('show');
+            });
+        }
+    });
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 import { hideLoader } from "./loader.js";
+import { dropdown } from "./dropdown.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     if (document.readyState === "complete") {
         hideLoader();
     } else window.addEventListener('load', hideLoader);
+    dropdown();
 });
